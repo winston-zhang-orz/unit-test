95586e3b9c28d213b85da4ab114c45b56c044a1d
winston-zhang-orz <73474183+winston-zhang-orz@users.noreply.github.com>
2025-03-23 23:41:43 +0800

    Update test_math_operation.c

--- ceedling/test/test_math_operation.c
+++ ceedling/test/test_math_operation.c
@@ -2,6 +2,8 @@
 #include "unity.h"
 #include "math_operation.h"
 
+#include <limits.h>
+
 void setUp(void) {
     // Set up code if needed
 }
@@ -13,7 +15,13 @@ void tearDown(void) {
 void test_add(void) {
     TEST_ASSERT_EQUAL(5, add(2, 3));
     TEST_ASSERT_EQUAL(-1, add(2, -3));
-    TEST_ASSERT_EQUAL(1, add(3, -3));
+    TEST_ASSERT_EQUAL(0, add(3, -3));
+    
+      // Additional boundary tests
+    TEST_ASSERT_EQUAL(0, add(0, 0));
+    TEST_ASSERT_EQUAL(INT_MAX, add(INT_MAX, 0));
+    TEST_ASSERT_EQUAL(INT_MIN, add(INT_MIN, 0));
+    TEST_ASSERT_EQUAL(-1, add(INT_MAX, INT_MIN)); // This may overflow, consider handling overflow in the add function
 }
 
 void test_subtract(void) {
